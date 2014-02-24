164d51d0e50c49d2cbc3de63da7812fd732b1a19
Cristian Acuña <cristian.acuna.elo1@gmail.com>
2014-02-24 20:46:58 -0300

    Se intenta agregar sin éxito verificación de efecto fotoeléctrico y dispersión de Compton.

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -8,13 +8,16 @@
 #include "G4Step.hh"
 #include "G4RunManager.hh"
 #include "G4ParticleTypes.hh"
+#include "G4String.hh"
+
+#include "G4VProcess.hh"
+#include "G4PhotoElectricEffect.hh"
+#include "G4ComptonScattering.hh"
 
 SteppingAction::SteppingAction()                                         
 {
-  detector = (DetectorConstruction*)
-             G4RunManager::GetRunManager()->GetUserDetectorConstruction();
-  eventAction = (EventAction*)
-                G4RunManager::GetRunManager()->GetUserEventAction();               
+  detector = (DetectorConstruction*) G4RunManager::GetRunManager()->GetUserDetectorConstruction();
+  eventAction = (EventAction*) G4RunManager::GetRunManager()->GetUserEventAction();               
  }
 
  SteppingAction::~SteppingAction()
@@ -28,15 +31,24 @@ void SteppingAction::UserSteppingAction(const G4Step* aStep)
   
   // collect energy and track length step by step
   G4double edep = aStep->GetTotalEnergyDeposit();
-  // G4double diffedep = aStep->GetDeltaEnergy();
-  
-  
+    
   G4double stepl = 0.;
   if (aStep->GetTrack()->GetDefinition()->GetPDGCharge() != 0.)
     stepl = aStep->GetStepLength();
   if(aStep->GetTrack()->GetDefinition()->GetParticleType() == "gamma" &&
-     edep)
+     //(
+     // (aStep->GetTrack()->GetCreatorProcess()->GetProcessName() == "phot") ||
+     // (aStep->GetTrack()->GetCreatorProcess()->GetProcessName() == "compt")
+     // )
+    edep)
     {
+      /* DEBUG =======================================================*/
+      G4cout << "DEBUG: Hello World!\n";
+      G4String out = aStep->GetTrack()->GetCreatorProcess()->GetProcessName();
+      if (out != NULL) G4cout << out << G4endl;
+      G4cout << "Energy deposited:\t" << edep << G4endl;
+      /*==============================================================*/
+
       if (volume == detector->GetTrigger())	eventAction->AddNOTP();
       if (volume == detector->GetSensor())	eventAction->AddNOSP();
     }      
